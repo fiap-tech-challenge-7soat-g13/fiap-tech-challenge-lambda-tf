6e5a99b4c12dd052058cd5ddeafb26143e4f97b2
Graziela Goedert de Souza <graziela.souza@grupoboticario.com.br>
2024-10-03 23:51:00 -0300

    Adicionando api-gateway

--- terraform/main.tf
+++ terraform/main.tf
@@ -62,6 +62,91 @@ resource "aws_cognito_user_pool_client" "client" {
   explicit_auth_flows = ["ALLOW_ADMIN_USER_PASSWORD_AUTH", "ALLOW_REFRESH_TOKEN_AUTH", "ALLOW_USER_SRP_AUTH", "ALLOW_USER_PASSWORD_AUTH", "ALLOW_CUSTOM_AUTH"]
 }
 
+resource "aws_security_group" "auth_sign_up" {
+  name   = "auth_sign_up"
+  vpc_id = data.terraform_remote_state.tech-challenge.outputs.vpc_id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+module "lambda_auth_sign_up" {
+  source  = "terraform-aws-modules/lambda/aws"
+  version = "7.2.2"
+
+  function_name = "auth-sign-up"
+  handler       = "lambda_function.lambda_handler"
+  runtime       = local.runtime
+
+  source_path = {
+    path             = "../src/sign-up"
+    pip_requirements = true
+  }
+
+  environment_variables = {
+    USER_POOL_ID      = aws_cognito_user_pool.user_pool.id
+    LOAD_BALANCER_DNS = data.aws_lb.orders_load_balancer.dns_name
+    TARGET_PORT       = var.target_group_port
+  }
+
+  attach_policy_statements = true
+  policy_statements = {
+    cognito = {
+      effect = "Allow"
+      actions = [
+        "cognito-idp:AdminCreateUser",
+        "cognito-idp:AdminAddUserToGroup"
+      ]
+      resources = [
+        aws_cognito_user_pool.user_pool.arn
+      ]
+    }
+  }
+
+  vpc_subnet_ids         = data.terraform_remote_state.tech-challenge.outputs.private_subnets
+  vpc_security_group_ids = [aws_security_group.auth_sign_up.id]
+  attach_network_policy  = true
+}
+
+module "lambda_auth_sign_in" {
+  source  = "terraform-aws-modules/lambda/aws"
+  version = "7.2.2"
+
+  function_name = "auth-sign-in"
+  handler       = "lambda_function.lambda_handler"
+  runtime       = local.runtime
+
+  source_path = "../src/sign-in"
+
+  environment_variables = {
+    USER_POOL_ID = aws_cognito_user_pool.user_pool.id
+    CLIENT_ID    = aws_cognito_user_pool_client.client.id
+  }
+
+  attach_policy_statements = true
+  policy_statements = {
+    cognito = {
+      effect = "Allow"
+      actions = [
+        "cognito-idp:AdminInitiateAuth"
+      ]
+      resources = [
+        aws_cognito_user_pool.user_pool.arn
+      ]
+    }
+  }
+
+  tags = var.tags
+
+  depends_on = [
+    aws_cognito_user_pool.user_pool
+  ]
+}
+
 data "aws_vpc" "default" {
   default = true
 }
