c1ef86a4bd674b426c4aea3949cfb2b832bb345f
Graziela Goedert de Souza <graziela.souza@grupoboticario.com.br>
2024-10-04 01:04:57 -0300

    Adicionando api-gateway

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,16 +2,6 @@ locals {
   runtime = "python3.12"
 }
 
-data "terraform_remote_state" "tech-challenge" {
-  backend = "s3"
-
-  config = {
-    bucket = "terraform-state-829dbe75"
-    key    = "fiap-tech-challenge-lambda-tf"
-    region = "us-east-1"
-  }
-}
-
 resource "null_resource" "always_run" {
   triggers = {
     timestamp = timestamp()
@@ -72,9 +62,13 @@ resource "aws_cognito_user_pool_client" "client" {
   explicit_auth_flows = ["ALLOW_ADMIN_USER_PASSWORD_AUTH", "ALLOW_REFRESH_TOKEN_AUTH", "ALLOW_USER_SRP_AUTH", "ALLOW_USER_PASSWORD_AUTH", "ALLOW_CUSTOM_AUTH"]
 }
 
+data "aws_vpc" "default" {
+  default = true
+}
+
 resource "aws_security_group" "auth_sign_up" {
   name   = "auth_sign_up"
-  vpc_id = data.terraform_remote_state.tech-challenge.outputs.vpc_id
+  vpc_id = data.aws_vpc.default.id
 
   egress {
     from_port   = 0
@@ -99,7 +93,6 @@ module "lambda_auth_sign_up" {
 
   environment_variables = {
     USER_POOL_ID      = aws_cognito_user_pool.user_pool.id
-    LOAD_BALANCER_DNS = data.aws_lb.orders_load_balancer.dns_name
     TARGET_PORT       = var.target_group_port
   }
 
@@ -109,6 +102,7 @@ module "lambda_auth_sign_up" {
       effect = "Allow"
       actions = [
         "cognito-idp:AdminCreateUser",
+        "cognito-idp:AdminGetUser",
         "cognito-idp:AdminAddUserToGroup"
       ]
       resources = [
@@ -117,17 +111,8 @@ module "lambda_auth_sign_up" {
     }
   }
 
-  vpc_subnet_ids         = data.terraform_remote_state.tech-challenge.outputs.private_subnets
   vpc_security_group_ids = [aws_security_group.auth_sign_up.id]
   attach_network_policy  = true
-}
-
-data "aws_lambda_function" "auth_sign_in" {
-  function_name = "auth-sign-in"
-}
-
-data "aws_lambda_function" "auth_sign_up" {
-  function_name = "auth-sign-up"
 }
 
 module "lambda_auth_sign_in" {
@@ -150,7 +135,8 @@ module "lambda_auth_sign_in" {
     cognito = {
       effect = "Allow"
       actions = [
-        "cognito-idp:AdminInitiateAuth"
+        "cognito-idp:AdminInitiateAuth",
+        "cognito-idp:AdminGetUser",
       ]
       resources = [
         aws_cognito_user_pool.user_pool.arn
@@ -163,74 +149,4 @@ module "lambda_auth_sign_in" {
   depends_on = [
     aws_cognito_user_pool.user_pool
   ]
-}
-
-data "aws_vpc" "default" {
-  default = true
-}
-
-data "aws_lb" "orders_load_balancer" {
-  name = "orders-load-balancer"
-}
-
-resource "aws_api_gateway_vpc_link" "vpc_link" {
-  name        = "self-order-management-api"
-  target_arns = [data.aws_lb.orders_load_balancer.arn]
-}
-
-resource "aws_api_gateway_rest_api" "api_gateway" {
-  name = "Self-Order Management API"
-
-  body = templatefile(
-    "../src/api/api.yaml",
-    {
-      target_group_port          = var.target_group_port
-      dns_name                   = data.aws_lb.orders_load_balancer.dns_name
-      vpc_link_id                = aws_api_gateway_vpc_link.vpc_link.id
-      api_gateway_role           = aws_iam_role.api_gateway_lambda.arn
-      lambda_auth_sign_up_arn    = data.aws_lambda_function.auth_sign_up.invoke_arn
-      lambda_auth_sign_in_arn    = data.aws_lambda_function.auth_sign_in.invoke_arn
-    }
-  )
-
-  endpoint_configuration {
-    types = ["REGIONAL"]
-  }
-}
-
-data "aws_iam_policy_document" "api_gateway" {
-  statement {
-    effect = "Allow"
-
-    principals {
-      type        = "*"
-      identifiers = ["*"]
-    }
-
-    actions   = ["execute-api:Invoke"]
-    resources = ["${aws_api_gateway_rest_api.api_gateway.execution_arn}/*/*/*"]
-  }
-}
-
-resource "aws_api_gateway_rest_api_policy" "api_gateway" {
-  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
-  policy      = data.aws_iam_policy_document.api_gateway.json
-}
-
-resource "aws_api_gateway_deployment" "api_gateway" {
-  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
-
-  triggers = {
-    redeployment = sha1(jsonencode(aws_api_gateway_rest_api.api_gateway.body))
-  }
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-resource "aws_api_gateway_stage" "api_gateway" {
-  stage_name    = "live"
-  deployment_id = aws_api_gateway_deployment.api_gateway.id
-  rest_api_id   = aws_api_gateway_rest_api.api_gateway.id
 }
