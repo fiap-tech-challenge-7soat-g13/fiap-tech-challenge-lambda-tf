7c8273be44ab5c3c14da3aada6ee967f2ba1f906
Graziela Goedert de Souza <graziela.souza@grupoboticario.com.br>
2024-10-04 14:29:20 -0300

    Adicionando api-gateway

--- terraform/main.tf
+++ terraform/main.tf
@@ -150,3 +150,101 @@ module "lambda_auth_sign_in" {
     aws_cognito_user_pool.user_pool
   ]
 }
+
+resource "aws_subnet" "subnet_a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+}
+
+data "aws_lambda_function" "auth_sign_in" {
+  function_name = "auth-sign-in"
+}
+
+data "aws_lambda_function" "auth_sign_up" {
+  function_name = "auth-sign-up"
+}
+
+data "aws_subnets" "private_subnets" {
+  filter {
+    name   = "tag:Name"
+    values = ["*private*"]
+  }
+}
+
+data "aws_subnets" "public_subnets" {
+  filter {
+    name   = "tag:Name"
+    values = ["subnet-03b29d1a8c9ff8531", "subnet-0942a186952fcd474"]
+  }
+}
+
+resource "aws_lb" "apigateway" {
+  name                       = "orders-load-balancer"
+  internal                   = true
+  load_balancer_type         = "network"
+  subnets                    = data.aws_subnets.public_subnets.ids
+  enable_deletion_protection = true
+}
+
+resource "aws_api_gateway_vpc_link" "vpc_link" {
+  name        = "self-order-management-api"
+  target_arns = [aws_lb.apigateway.arn]
+}
+
+resource "aws_api_gateway_rest_api" "api_gateway" {
+  name = "Self-Order Management API"
+
+  body = templatefile(
+    "../src/api/api.yaml",
+    {
+      target_group_port          = var.target_group_port
+      dns_name                   = aws_lb.apigateway.dns_name
+      vpc_link_id                = aws_api_gateway_vpc_link.vpc_link.id
+      api_gateway_role           = aws_iam_role.api_gateway_lambda.arn
+      lambda_auth_sign_up_arn    = data.aws_lambda_function.auth_sign_up.invoke_arn
+      lambda_auth_sign_in_arn    = data.aws_lambda_function.auth_sign_in.invoke_arn
+    }
+  )
+
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
+}
+
+data "aws_iam_policy_document" "api_gateway" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+
+    actions   = ["execute-api:Invoke"]
+    resources = ["${aws_api_gateway_rest_api.api_gateway.execution_arn}/*/*/*"]
+  }
+}
+
+resource "aws_api_gateway_rest_api_policy" "api_gateway" {
+  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
+  policy      = data.aws_iam_policy_document.api_gateway.json
+}
+
+resource "aws_api_gateway_deployment" "api_gateway" {
+  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
+
+  triggers = {
+    redeployment = sha1(jsonencode(aws_api_gateway_rest_api.api_gateway.body))
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_api_gateway_stage" "api_gateway" {
+  stage_name    = "live"
+  deployment_id = aws_api_gateway_deployment.api_gateway.id
+  rest_api_id   = aws_api_gateway_rest_api.api_gateway.id
+}
