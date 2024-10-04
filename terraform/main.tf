21e8fa05ec620ece5efab8fe225813a8fcda7c3a
Graziela Goedert de Souza <graziela.souza@grupoboticario.com.br>
2024-10-04 14:30:51 -0300

    Adicionando api-gateway

--- terraform/main.tf
+++ terraform/main.tf
@@ -151,6 +151,10 @@ module "lambda_auth_sign_in" {
   ]
 }
 
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
 resource "aws_subnet" "subnet_a" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.1.0/24"
