1b1f92e7e142bafd757ab5a1e3e696bab0369205
Cristiano de Barros <cristiano.barros13@gmail.com>
2024-12-05 01:57:55 -0300

    Merge dos API Gateways

--- terraform/gateway.tf
+++ terraform/gateway.tf
@@ -1,19 +1,58 @@
+data "aws_lb" "default" {
+  tags = {
+    "kubernetes.io/service-name" = "istio-ingress/istio-ingressgateway"
+  }
+}
+resource "aws_api_gateway_vpc_link" "default" {
+  name        = "default"
+  target_arns = [data.aws_lb.default.arn]
+}
 resource "aws_api_gateway_rest_api" "default" {
   name = "default"
   endpoint_configuration {
     types = ["REGIONAL"]
   }
 }
+resource "aws_api_gateway_resource" "default" {
+  rest_api_id = aws_api_gateway_rest_api.default.id
+  parent_id   = aws_api_gateway_rest_api.default.root_resource_id
+  path_part   = "{proxy+}"
+}
 resource "aws_api_gateway_resource" "login" {
   rest_api_id = aws_api_gateway_rest_api.default.id
   parent_id   = aws_api_gateway_rest_api.default.root_resource_id
   path_part   = "login"
+}
+resource "aws_api_gateway_method" "default" {
+  rest_api_id   = aws_api_gateway_rest_api.default.id
+  resource_id   = aws_api_gateway_resource.default.id
+  http_method   = "ANY"
+  authorization = "NONE"
+  request_parameters = {
+    "method.request.path.proxy" = true
+  }
 }
 resource "aws_api_gateway_method" "method" {
   rest_api_id   = aws_api_gateway_rest_api.default.id
   resource_id   = aws_api_gateway_resource.login.id
   http_method   = "POST"
   authorization = "NONE"
+}
+resource "aws_api_gateway_integration" "default" {
+  rest_api_id             = aws_api_gateway_rest_api.default.id
+  resource_id             = aws_api_gateway_resource.default.id
+  http_method             = "ANY"
+  integration_http_method = "ANY"
+  type                    = "HTTP_PROXY"
+  uri                     = "http://${data.aws_lb.default.dns_name}/{proxy}"
+  passthrough_behavior    = "WHEN_NO_MATCH"
+  content_handling        = "CONVERT_TO_TEXT"
+  request_parameters = {
+    "integration.request.path.proxy"    = "method.request.path.proxy"
+    "integration.request.header.Accept" = "'application/json'"
+  }
+  connection_type = "VPC_LINK"
+  connection_id   = aws_api_gateway_vpc_link.default.id
 }
 resource "aws_api_gateway_integration" "lambda" {
   rest_api_id             = aws_api_gateway_rest_api.default.id
@@ -51,9 +90,11 @@ resource "aws_lambda_permission" "default" {
   source_arn    = "${aws_api_gateway_rest_api.default.execution_arn}/*/*"
 }
 resource "aws_api_gateway_deployment" "default" {
-  stage_name  = "production"
   rest_api_id = aws_api_gateway_rest_api.default.id
-  depends_on = [
-    aws_api_gateway_integration.lambda
-  ]
+  depends_on = [aws_api_gateway_integration.default, aws_api_gateway_integration.lambda]
+}
+resource "aws_api_gateway_stage" "default" {
+  deployment_id = aws_api_gateway_deployment.default.id
+  rest_api_id   = aws_api_gateway_rest_api.default.id
+  stage_name    = "dev"
 }
