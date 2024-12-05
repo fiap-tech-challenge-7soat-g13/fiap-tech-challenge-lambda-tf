fd693d3e9732ead8f29aaa9156d91cb28664f308
Cristiano de Barros <cristiano.barros13@gmail.com>
2024-12-05 12:32:22 -0300

    Domínio customizado

--- terraform/gateway.tf
+++ terraform/gateway.tf
@@ -3,6 +3,12 @@ data "aws_lb" "default" {
     "kubernetes.io/service-name" = "istio-ingress/istio-ingressgateway"
   }
 }
+data "aws_acm_certificate" "default" {
+  domain = var.domain
+}
+data "aws_route53_zone" "default" {
+  name = var.domain
+}
 resource "aws_api_gateway_vpc_link" "default" {
   name        = "default"
   target_arns = [data.aws_lb.default.arn]
@@ -97,4 +103,23 @@ resource "aws_api_gateway_stage" "default" {
   deployment_id = aws_api_gateway_deployment.default.id
   rest_api_id   = aws_api_gateway_rest_api.default.id
   stage_name    = "dev"
+}
+resource "aws_api_gateway_domain_name" "default" {
+  certificate_arn = data.aws_acm_certificate.default.arn
+  domain_name     = var.domain
+}
+resource "aws_api_gateway_base_path_mapping" "default" {
+  api_id      = aws_api_gateway_rest_api.default.id
+  stage_name  = aws_api_gateway_stage.default.stage_name
+  domain_name = aws_api_gateway_domain_name.default.domain_name
+}
+resource "aws_route53_record" "default" {
+  name    = aws_api_gateway_domain_name.default.domain_name
+  zone_id = data.aws_route53_zone.default.id
+  type    = "A"
+  alias {
+    name                   = aws_api_gateway_domain_name.default.cloudfront_domain_name
+    zone_id                = aws_api_gateway_domain_name.default.cloudfront_zone_id
+    evaluate_target_health = false
+  }
 }
